8a60dbb38fc7d7347841533bf0a75d192b4b071c
Manis <116066157+MMANISS@users.noreply.github.com>
2023-06-19 23:35:08 +0300

    First push

---
+++ Assets/CoinPicker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CoinPicker : MonoBehaviour
+{
+    private float coins = 0;
+    public TMP_Text coinsText;
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Coin")
+        {
+            coins++;
+            coinsText.text = coins.ToString();
+            Destroy(coll.gameObject);
+        }
+    }
+}
---
+++ Assets/PlayerScript2D.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript2D : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float HorizontalMove = 0f; //run
+    private bool FacingRight = true;
+
+    [Header("Player Movement Settings")]
+    [Range(0, 10f)] public float speed = 1f; //run
+    [Range(0, 15f)] public float jumpForce = 8f; //jump
+
+    [Header("Player Animation Settings")]
+    public Animator animator; //amitation
+
+    [Space]
+    [Header("Ground Checker Settings")]
+    public bool isGrounded = false;
+    [Range(-5f, 5f)] public float checkGroundOffsetY = -1.8f;
+    [Range(0, 5f)] public float checkGroundRadius = 0.3f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space)) //jump
+        {
+            rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        HorizontalMove = Input.GetAxisRaw("Horizontal") * speed; //run
+
+        animator.SetFloat("HorizontalMove", Mathf.Abs(HorizontalMove)); //animation
+
+        if (isGrounded == false)
+        {
+            animator.SetBool("Jumping", true); //animation
+        }
+        else
+        {
+            animator.SetBool("Jumping", false); //animation
+        }
+
+        if (HorizontalMove < 0 && FacingRight)
+        {
+            Flip();
+        }
+        else if (HorizontalMove > 0 && !FacingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 targetVelocity = new Vector2(HorizontalMove * 10f, rb.velocity.y); //run
+        rb.velocity = targetVelocity;
+
+        CheckGround();
+    }
+
+    private void Flip()
+    { 
+        FacingRight = !FacingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+
+    }
+
+    private void CheckGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll
+            (new Vector2(transform.position.x, transform.position.y + checkGroundOffsetY), checkGroundRadius);
+        
+        if (colliders.Length > 1)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+}
